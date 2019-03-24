176dcfb55b7c30cb1a3a641bfc98ef876ae571e9
Bendy Zhang <zb@bndy.net>
2019-03-24 22:53:39 +0800

    test(loading): add missing unit tests

--- test/Loading.spec.ts
+++ test/Loading.spec.ts
@@ -1,4 +1,4 @@
-import { loading } from "../src/scripts/Loading";
+import { loading, loadingFor, Loading } from "../src/scripts/Loading";
 
 describe("Loading", () => {
     it("should render the loading element and can be closed", () => {
@@ -13,6 +13,30 @@ describe("Loading", () => {
         expect(document.querySelectorAll(".bn-loading-container").length).toBe(1);
         expect(document.querySelectorAll(".loading-text").length).toBe(1);
         el.hide();
+        expect(document.querySelectorAll(".bn-loading-container").length).toBe(0);
+    });
+
+    it("should render the loading element for specified element using constructor", () => {
+        const box = document.createElement("div");
+        box.setAttribute("id", "box");
+        document.body.appendChild(box);
+        const l = new Loading({
+            targetEl: document.querySelector("#box"),
+            inline: true,
+            color: "#ff0000",
+        }).show();
+        expect(document.querySelectorAll(".bn-loading-container").length).toBe(1);
+        l.hide();
+        expect(document.querySelectorAll(".bn-loading-container").length).toBe(0);
+    });
+
+    it("should render the loading element for specified element using `loadingFor` method", () => {
+        const box = document.createElement("div");
+        box.setAttribute("id", "box");
+        document.body.appendChild(box);
+        const l = loadingFor("#box");
+        expect(document.querySelectorAll(".bn-loading-container").length).toBe(1);
+        l.hide();
         expect(document.querySelectorAll(".bn-loading-container").length).toBe(0);
     });
 });
