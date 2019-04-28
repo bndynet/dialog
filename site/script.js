3f130cc057fc337ff2d0dd1a22f89f022cbf497e
Bendy Zhang <zb@bndy.net>
2019-04-28 21:38:41 +0800

    docs: add changelog to project site

--- site/script.js
+++ site/script.js
@@ -12,3 +12,14 @@ axios.get("../README.md").then(function(response) {
     }, 2000);
 });
 
+let urlChangelog = "../CHANGELOG.md";
+// always get the online CHANGELOG.md
+const exp = /https:\/\/(\w+).github.io\/([^\/]+)/i;
+const matches = location.href.match(exp);
+if (matches && matches.length >= 3) {
+    urlChangelog = "https://raw.githubusercontent.com/" + matches[1] + "/" + matches[2] + "/master/CHANGELOG.md";
+}
+axios.get(urlChangelog).then(function(response) {
+    var converter = new showdown.Converter();
+    document.getElementById("changelog-body").innerHTML = converter.makeHtml(response.data);
+});
